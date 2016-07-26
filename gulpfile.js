5f5ad34d3eae8eecc47a4f747c526ba8f9deb87b
Matthew Paul <mclydep@gmail.com>
2016-07-25 19:06:25 -0500

    :construction_worker: Refactored Gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,44 @@ var autoprefixer = require('gulp-autoprefixer');
 var browserSync  = require('browser-sync').create();
 var deploy       = require('gulp-gh-pages');
 
-// Compile SCSS into CSS, write sourcemaps, run autoprefixer, minify CSS & auto-inject into browsers
-gulp.task('styles', function() {
-  return gulp.src('./_assets/styles/style.scss')
+// Compile SCSS into CSS, sourcemaps, autoprefixer, cssnano + auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src('_styles/scss/style.scss')
   .pipe(sourcemaps.init())
   .pipe(sass())
   .pipe(sourcemaps.write())
   .pipe(autoprefixer())
+  .pipe(gulp.dest('_styles/css'))
   .pipe(nano({discardComments: {removeAll: true}}))
-  .pipe(gulp.dest('./_site/assets/css'))
+  .pipe(gulp.dest('_site/assets/css'))
   .pipe(browserSync.stream());
 });
 
-// Build incrementally with jekyl config + local config
+// Build incrementally with _config.yml + local_config.yml for development
 gulp.task('local-build', shell.task(['bundle exec jekyll build --config _config.yml,local_config.yml']));
 
-// Start a static server + watch tasks
-gulp.task('serve', ['styles', 'local-build'], function() {
-
+// Start a local server with browser-sync + watch for changes
+gulp.task('serve', ['sass', 'local-build'], function() {
     browserSync.init({
         server: { baseDir: '_site/' }
     });
 
-    gulp.watch('_assets/styles/**/*.scss', ['styles']);
+    gulp.watch('_styles/scss/**/*.scss', ['sass']);
     gulp.watch(['index.md', 'about/index.md'], ['local-build']);
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-// Gulp: Run styles, local-build, and serve
+// Run sass, local-build, and serve
 gulp.task('default', ['serve']);
 
-// Build once with only jekyll config
-gulp.task('jekyll-build', shell.task(['bundle exec jekyll build']));
+// Build once with only _config.yml for production
+gulp.task('production-build', shell.task(['bundle exec jekyll build']));
 
 // Deploy _site to gh-pages
-gulp.task('deploy-gh-pages', ['jekyll-build'], function () {
+gulp.task('deploy-gh-pages', ['production-build'], function () {
   return gulp.src('./_site/**/*')
     .pipe(deploy())
 });
 
-// Gulp Deploy: Run jekyll-build, and deploy-gh-pages
+// Run production-build, and deploy-gh-pages
 gulp.task('deploy', ['deploy-gh-pages']);
